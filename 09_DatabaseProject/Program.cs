72a5d3a607579c15849a4aec8fd0de372426ba01
oguzhankureksiz <kureksizoguzhan@gmail.com>
2025-03-10 02:05:57 +0300

    9.gün tamamlandı. Ado .net çalışıldı.

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ado .net
+
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("--------------------------------------------------------");
+
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-PMH598B\SQLEXPRESS01;Initial Catalog=EgitimKampiDb;Integrated Security=True");
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("Select * From TblCategory",connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            connection.Close();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
